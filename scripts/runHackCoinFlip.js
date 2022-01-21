68f0f6dfb47fb433c186f2d3f4e7bb203c1450fd
John Matthews <JMMatthews@Protonmail.com>
2022-01-20 23:30:25 -0600

    Add original CoinFlip address to deploy with constructor

--- scripts/runHackCoinFlip.js
+++ scripts/runHackCoinFlip.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const hackCoinFlipFactory = await hre.ethers.getContractFactory("HackCoinFlip"),
-  hackContract = await hackCoinFlipFactory.deploy();
+  hackContract = await hackCoinFlipFactory.deploy("0xE681d8234A3a3217B8c2478a7a29a44428Ef442b");
   await hackContract.deployed();
   console.log("Contract deployed to: " + hackContract.address);
 
