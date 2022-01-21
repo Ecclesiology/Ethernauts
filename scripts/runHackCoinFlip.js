a8c9788ad4207f2d660322fa2b200a9104553c3b
John Matthews <JMMatthews@Protonmail.com>
2022-01-20 23:47:52 -0600

    Remove win counter

--- scripts/runHackCoinFlip.js
+++ scripts/runHackCoinFlip.js
@@ -7,7 +7,6 @@ const main = async () => {
   for(let i = 0; i <= 10; i++){
     let txn = await hackContract.hack();
     await txn.wait();
-    console.log("Total wins: " + await hackContract.consecutiveWins());
   }
 }
 
