a623a38a4cd86e37f7079e1412383f61c902131e
John Matthews <JMMatthews@Protonmail.com>
2022-01-21 00:18:31 -0600

    Adjust smart contract address for Telephone

--- scripts/runHackTelephone.js
+++ scripts/runHackTelephone.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const hackTelephoneFactory = await hre.ethers.getContractFactory("HackTelephone"),
-  hackContract = await hackTelephoneFactory.deploy("0xE681d8234A3a3217B8c2478a7a29a44428Ef442b");
+  hackContract = await hackTelephoneFactory.deploy("0x023e90580fCda1c544C7D710c34fb9630041fEd9");
   await hackContract.deployed();
   console.log("Contract deployed to: " + hackContract.address);
 
