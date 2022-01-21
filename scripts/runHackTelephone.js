740a2f6b375a0d1dca121fce7ec682e10fc372c6
John Matthews <JMMatthews@Protonmail.com>
2022-01-21 00:17:51 -0600

    Create HackTelephone contract factory

--- scripts/runHackTelephone.js
+++ scripts/runHackTelephone.js
@@ -1,5 +1,5 @@
 const main = async () => {
-  const hackTelephoneFactory = await hre.ethers.getContractFactory("HackCoinFlip"),
+  const hackTelephoneFactory = await hre.ethers.getContractFactory("HackTelephone"),
   hackContract = await hackTelephoneFactory.deploy("0xE681d8234A3a3217B8c2478a7a29a44428Ef442b");
   await hackContract.deployed();
   console.log("Contract deployed to: " + hackContract.address);
