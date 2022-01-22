89b0b48659043c64aaf2ebb3ffd2e13ebff01900
John Matthews <JMMatthews@Protonmail.com>
2022-01-21 19:14:08 -0600

    Fetch abi asyncronously

--- scripts/runHackToken.js
+++ scripts/runHackToken.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-const tokenAbi = hre.artifacts.readArtifact("Token")
+
 const provider = new hre.ethers.providers.JsonRpcProvider( process.env.RINKEBY_URL );
 const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 
 const main = async () => {
+  const tokenAbi = await hre.artifacts.readArtifact("Token")
   const hackToken = await new hre.ethers.Contract("0xF221B392AB7F89D32b941fFdE6b3Ea271AA30587", tokenAbi.abi, wallet);
   console.log("Token contract: " + hackToken.address);
 
