0778e39a6e5c00df4ea5ca41c402884526f43cad
John M <neshamon@protonmail.com>
2022-01-25 16:49:16 -0600

    Stop deploying contract with ether. Pass ether directly to contract

--- scripts/runHackKing.js
+++ scripts/runHackKing.js
@@ -9,11 +9,11 @@ const main = async () => {
   console.log("King contract: " + kingContract.address);
 
   const hackKingFactory = await hre.ethers.getContractFactory("HackKing");
-  const hackKingContract = await hackKingFactory.deploy('0x728a90D5dF447c3298786985b24Af810cb4Cde71', {value: 3010000});
+  const hackKingContract = await hackKingFactory.deploy('0x728a90D5dF447c3298786985b24Af810cb4Cde71');
   await hackKingContract.deployed();
   console.log(`HackKing contract: ${hackKingContract.address}`)
 
-  let txn = await hackKingContract.crownMe(3000000);
+  let txn = await hackKingContract.crownMe({value: "100000000000000000"});
   console.log("Selecting new king...")
   await txn.wait();
   console.log("The new king has been crowned.");
