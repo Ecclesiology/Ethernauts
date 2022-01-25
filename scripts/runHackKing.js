e57923fdeac7d212e0e74deeffca77f53984aa01
John M <neshamon@protonmail.com>
2022-01-25 11:53:40 -0600

    Update ABI

--- scripts/runHackKing.js
+++ scripts/runHackKing.js
@@ -4,7 +4,7 @@ const provider = new hre.ethers.providers.JsonRpcProvider( process.env.RINKEBY_U
 const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 
 const main = async () => {
-  const kingAbi = await hre.artifacts.readArtifact("contracts/Delegate\.sol:Delegation");
+  const kingAbi = await hre.artifacts.readArtifact("King");
   const kingContract = await new hre.ethers.Contract("0xd331155cDAe18eB737864989016E4be091aB9E3b", kingAbi.abi, wallet);
   console.log("Delegate contract: " + kingContract.address);
 
