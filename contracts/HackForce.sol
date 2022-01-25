e52184f5c8e12f9ffffa33166b85145603da0492
John M <neshamon@protonmail.com>
2022-01-24 20:09:03 -0600

    Create attack contract

---
+++ contracts/HackForce.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract HackForce {
+  address public forceAddress
+
+  constructor(address _forceAddress){
+    forceAddress = _forceAddress
+  }
+}
