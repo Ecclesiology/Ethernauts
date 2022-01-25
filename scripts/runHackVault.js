4bbf259857595f2d7113970697c745fcdf19f7ae
John M <neshamon@protonmail.com>
2022-01-24 22:06:02 -0600

    Set up runHackVault

---
+++ scripts/runHackVault.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const provider = new hre.ethers.providers.JsonRpcProvider( process.env.RINKEBY_URL );
+const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
+
+const main = async () => {
+  const abiEncoder = hre.ethers.utils.defaultAbiCoder
+  const vaultAbi = await hre.artifacts.readArtifact("Vault");
+  const vaultContract = await new hre.ethers.Contract("0x7Ca9Ffa0792BD500fFaCD04108eB0DCd58Da4432", vaultAbi.abi, wallet);
+  console.log("Delegate contract: " + vaultContract.address);
+
+
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
