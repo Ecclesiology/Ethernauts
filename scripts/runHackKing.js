faaf89a610df03dabda68834fd16fa884e9c48a7
John M <neshamon@protonmail.com>
2022-01-25 11:53:05 -0600

    Rename variables

--- scripts/runHackKing.js
+++ scripts/runHackKing.js
@@ -4,13 +4,13 @@ const provider = new hre.ethers.providers.JsonRpcProvider( process.env.RINKEBY_U
 const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 
 const main = async () => {
-  const delegateAbi = await hre.artifacts.readArtifact("contracts/Delegate\.sol:Delegation");
-  const delegateContract = await new hre.ethers.Contract("0xd331155cDAe18eB737864989016E4be091aB9E3b", delegateAbi.abi, wallet);
-  console.log("Delegate contract: " + delegateContract.address);
+  const kingAbi = await hre.artifacts.readArtifact("contracts/Delegate\.sol:Delegation");
+  const kingContract = await new hre.ethers.Contract("0xd331155cDAe18eB737864989016E4be091aB9E3b", kingAbi.abi, wallet);
+  console.log("Delegate contract: " + kingContract.address);
 
 
   await wallet.sendTransaction({
-    to: delegateContract.address,
+    to: kingContract.address,
     from: wallet.address,
     data: "0xdd365b8b0000000000000000000000000000000000000000000000000000000000000000",
     gasLimit: "100000"
