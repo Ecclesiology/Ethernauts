6b475c7dfc09ab4f4fedd0f01ddbb1be0f3bf025
John M <neshamon@protonmail.com>
2022-01-24 21:34:52 -0600

    Deploy contract with value

--- scripts/runHackForce.js
+++ scripts/runHackForce.js
@@ -1,12 +1,12 @@
 const main = async () => {
-  const hackForceFactory = await hre.ethers.getContractFactory("HackForce"),
-  hackForce = await hackForceFactory.deploy('0x2A6Aec7F39856Efc74544a53601d228Eb5c43698');
-  await hackForce.deployed();
-  console.log("Contract deployed to: " + hackForce.address);
+  const hackForceFactory = await hre.ethers.getContractFactory("HackForce");
+  const hackForceContract = await hackForceFactory.deploy('0x2A6Aec7F39856Efc74544a53601d228Eb5c43698', {value: 1000});
+  await hackForceContract.deployed();
+  console.log("Contract deployed to: " + hackForceContract.address);
 
-  let txn = await hackForce.forceSend();
+  let txn = await hackForceContract.forceSend();
   await txn.wait();
-  console.log(`Ether successfully sent.`)
+  console.log("Ether successfully sent.")
 }
 
 
