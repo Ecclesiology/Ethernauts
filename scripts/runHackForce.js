0acb1f63c8ff124add14bb36ee8feabdd90cddf8
John M <neshamon@protonmail.com>
2022-01-24 20:13:16 -0600

    Update contract address

--- scripts/runHackForce.js
+++ scripts/runHackForce.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const hackForceFactory = await hre.ethers.getContractFactory("HackForce"),
-  hackForce = await hackForceFactory.deploy("0xE681d8234A3a3217B8c2478a7a29a44428Ef442b");
+  hackForce = await hackForceFactory.deploy('0x2A6Aec7F39856Efc74544a53601d228Eb5c43698');
   await hackForce.deployed();
   console.log("Contract deployed to: " + hackForce.address);
 
