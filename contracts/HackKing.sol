5bd2f1d3de96c0027dbb584f50ecbb9d8ecc8bc8
John M <neshamon@protonmail.com>
2022-01-25 12:50:12 -0600

    Set up constructor

--- contracts/HackKing.sol
+++ contracts/HackKing.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.6.0;
 
 contract HackKing {
-  
+  address public kingContract;
+
+  constructor(address public _kingContract) public payable {
+    kingContract = _kingContract
+  }
+
+  function crownMe() {
+
+  }
 
 }
