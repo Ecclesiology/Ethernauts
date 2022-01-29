6d6bb6704233ef35a5ff35c933f38c8671daba90
John M <neshamon@protonmail.com>
2022-01-28 20:01:37 -0600

    Update contract name

--- scripts/runHackReenrance.js
+++ scripts/runHackReenrance.js
@@ -5,8 +5,8 @@ const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 
 const main = async () => {
   const reentranceAbi = await hre.artifacts.readArtifact("Reentrance");
-  const kingContract = await new hre.ethers.Contract("0x311Dda1bD3289a11dBECA53f0103631a9AcdF24C", reentranceAbi.abi, wallet);
-  console.log("Reentrance contract: " + kingContract.address);
+  const reentranceContract = await new hre.ethers.Contract("0x311Dda1bD3289a11dBECA53f0103631a9AcdF24C", reentranceAbi.abi, wallet);
+  console.log("Reentrance contract: " + reentranceContract.address);
 
   const hackReentranceFactory = await hre.ethers.getContractFactory("HackReentrance");
   const hackReentranceContract = await hackReentranceFactory.deploy("0x311Dda1bD3289a11dBECA53f0103631a9AcdF24C", {value: "100000000000000000"});
@@ -17,7 +17,7 @@ const main = async () => {
   console.log("Donating...")
   await txn.wait();
   console.log("Donation complete.");
-  console.log(`The current king is ${await kingContract._king()}`)
+  console.log(`The current king is ${await reentranceContract._king()}`)
 }
 
 const runMain = async () => {
