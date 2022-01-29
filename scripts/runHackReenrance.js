c30464219be6fc31a555eafce463d7acedf4277f
John M <neshamon@protonmail.com>
2022-01-28 20:06:53 -0600

    Make constructor value a number

--- scripts/runHackReenrance.js
+++ scripts/runHackReenrance.js
@@ -9,7 +9,7 @@ const main = async () => {
   console.log(`Reentrance contract: ${reentranceContract.address}`);
 
   const hackReentranceFactory = await hre.ethers.getContractFactory("HackReentrance");
-  const hackReentranceContract = await hackReentranceFactory.deploy("0x311Dda1bD3289a11dBECA53f0103631a9AcdF24C", {value: "100000000000000000"});
+  const hackReentranceContract = await hackReentranceFactory.deploy("0x311Dda1bD3289a11dBECA53f0103631a9AcdF24C", {value: 100000000000000000});
   await hackReentranceContract.deployed();
   console.log(`HackReentrance contract: ${hackReentranceContract.address}`)
 
