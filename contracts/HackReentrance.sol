ff690d31ff7d8bfa83b847fe92c9a17f06d2c9be
John M <neshamon@protonmail.com>
2022-01-28 19:29:09 -0600

    Implement Reentrance interface

--- contracts/HackReentrance.sol
+++ contracts/HackReentrance.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
+
+interface IReentrance {
+  function donate(address _to) public payable;
+  function withdraw(uint _amount) public;
+}
+
+contract HackReentrance {
+  IReentrance public reentrnaceAddress
+  
+
+  constructor(IReentance _reentranceAddress) public payable {
+    reentranceAddress
+  };
+
+
+}
