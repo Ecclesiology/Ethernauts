63ec61b0a53c25055d277b5350afebc4d115de76
John M <neshamon@protonmail.com>
2022-02-05 02:27:40 -0600

    Create constructor

--- contracts/HackElevator.sol
+++ contracts/HackElevator.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-interface Building {
-  function isLastFloor(uint) external returns (bool);
+interface IElevator {
+  function goTo(uint _floor) external;
 }
 
 contract HackElevator {
+  bool public switch;
+  IElevator elevator;
 
+  constructor(IElevator _elevator){
+    elevator = IElevator(_elevator);
+  }
 
 }
